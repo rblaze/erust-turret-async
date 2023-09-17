c46fcffc7ecf8f8d91b2926ffe420e5dcc11e306
Andrey Sverdlichenko <blaze@rusty.zone>
2023-09-16 23:07:33 -0400

    Configure servos

--- cross/app/src/board.rs
+++ cross/app/src/board.rs
@@ -3,29 +3,40 @@
 use crate::error::Error;
 use crate::system_time::Ticker;
 
-use stm32f1xx_hal::gpio::{Alternate, OpenDrain};
-use stm32f1xx_hal::gpio::{PB6, PB7};
+use num::rational::Ratio;
+use rtt_target::debug_rprintln;
+use servo::{Bounds, Servo};
+use stm32f1xx_hal::adc::Adc;
+use stm32f1xx_hal::gpio::{Alternate, OpenDrain, PushPull};
+use stm32f1xx_hal::gpio::{PA8, PA9, PB6, PB7};
 use stm32f1xx_hal::i2c::{BlockingI2c, I2c, Mode};
 use stm32f1xx_hal::pac;
 use stm32f1xx_hal::prelude::*;
-use stm32f1xx_hal::time::Hertz;
-use stm32f1xx_hal::timer::Timer;
+use stm32f1xx_hal::time::{Hertz, MilliSeconds};
+use stm32f1xx_hal::timer::{PwmChannel, Timer};
 use vl53l1x::{BootState, VL53L1X};
 
 // Set max available clock frequency.
 // Not important for CPU but audio PWM resolution is barely enough even this way.
 // In hindsight, should have used chip with DAC.
 const CLOCK_FREQ: u32 = 64_000_000;
+const SERVO_FREQ: Hertz = Hertz::Hz(50);
 
 pub type I2cScl = PB6<Alternate<OpenDrain>>;
 pub type I2cSda = PB7<Alternate<OpenDrain>>;
 pub type I2cBus = BlockingI2c<pac::I2C1, (I2cScl, I2cSda)>;
+pub type SensorServoPin = PA8<Alternate<PushPull>>;
+pub type LaserServoPin = PA9<Alternate<PushPull>>;
 
 pub type Sensor = VL53L1X<I2cBus>;
+pub type SensorServo = Servo<PwmChannel<pac::TIM1, 0>>;
+pub type LaserServo = Servo<PwmChannel<pac::TIM1, 1>>;
 
 pub struct Board {
     pub ticker: Ticker,
+    pub laser_servo: LaserServo,
     pub sensor: Sensor,
+    pub sensor_servo: SensorServo,
 }
 
 impl Board {
@@ -49,7 +60,50 @@ impl Board {
         let ticker = Ticker::new(Timer::syst(cp.SYST, &clocks));
 
         let mut afio = dp.AFIO.constrain();
+        let mut gpioa = dp.GPIOA.split();
         let mut gpiob = dp.GPIOB.split();
+
+        // Get servos range limit.
+        let mut adc = Adc::adc1(dp.ADC1, clocks);
+        let mut servo_range_ch = gpioa.pa1.into_analog(&mut gpioa.crl);
+        let adc_reading: u16 = adc.read(&mut servo_range_ch)?;
+        let adc_max = adc.max_sample();
+        adc.release(); // No longer needed
+
+        debug_rprintln!("range {} of {}", adc_reading, adc_max);
+        // Avoid too small range
+        let adc_value = adc_reading.max(adc_max / 10);
+        let adc_ratio = Ratio::new(adc_value, adc_max);
+
+        // Configure servos.
+        let sensor_servo_pin: SensorServoPin = gpioa.pa8.into_alternate_push_pull(&mut gpioa.crh);
+        let laser_servo_pin: LaserServoPin = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+
+        let (sensor_servo_pwm, laser_servo_pwm) = dp
+            .TIM1
+            .pwm_hz(
+                (sensor_servo_pin, laser_servo_pin),
+                &mut afio.mapr,
+                SERVO_FREQ,
+                &clocks,
+            )
+            .split();
+
+        debug_rprintln!("pwm max duty {}", sensor_servo_pwm.get_max_duty());
+
+        let period: MilliSeconds = SERVO_FREQ
+            .try_into_duration()
+            .ok_or(Error::InvalidDuration)?;
+        let period_ms = period.to_millis().try_into()?;
+
+        let bounds = Bounds::scale_from_period_ms(&sensor_servo_pwm, period_ms, adc_ratio)?;
+        debug_rprintln!("sensor {}", bounds);
+        let mut sensor_servo = Servo::new(sensor_servo_pwm, bounds);
+        sensor_servo.enable();
+
+        let bounds = Bounds::scale_from_period_ms(&laser_servo_pwm, period_ms, adc_ratio)?;
+        let mut laser_servo = Servo::new(laser_servo_pwm, bounds);
+        laser_servo.enable();
 
         // Configure I2C.
         let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
@@ -71,6 +125,11 @@ impl Board {
         }
         sensor.sensor_init()?;
 
-        Ok(Board { ticker, sensor })
+        Ok(Board {
+            ticker,
+            laser_servo,
+            sensor,
+            sensor_servo,
+        })
     }
 }
--- cross/app/src/error.rs
+++ cross/app/src/error.rs
@@ -1,12 +1,36 @@
 #![deny(unsafe_code)]
 
+use core::num::TryFromIntError;
+
 #[derive(Debug)]
 pub enum Error {
+    Conversion(TryFromIntError),
+    InvalidDuration,
     Sensor(vl53l1x::Error<stm32f1xx_hal::i2c::Error>),
+    Servo(servo::Error),
+    UnexpectedlyBlocks,
+}
+
+impl From<servo::Error> for Error {
+    fn from(servo_error: servo::Error) -> Self {
+        Error::Servo(servo_error)
+    }
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(error: TryFromIntError) -> Self {
+        Error::Conversion(error)
+    }
 }
 
 impl From<vl53l1x::Error<stm32f1xx_hal::i2c::Error>> for Error {
     fn from(sensor_error: vl53l1x::Error<stm32f1xx_hal::i2c::Error>) -> Self {
         Error::Sensor(sensor_error)
+    }
+}
+
+impl From<nb::Error<()>> for Error {
+    fn from(_: nb::Error<()>) -> Self {
+        Error::UnexpectedlyBlocks
     }
 }
